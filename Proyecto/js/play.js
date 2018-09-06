7685e1d78afb10bf952ea612541c93daac4c740c
eferrer686 <edfmgr@hotmail.com>
2018-09-05 19:24:25 -0500

    sass profile

--- Proyecto/js/play.js
+++ Proyecto/js/play.js
@@ -1,3 +1,18 @@
 function goToCreateParty(){
   window.location.href = "../html/createParty.html";
 }
+function goToFastGame(){
+  window.location.href = "../html/fastGame.html";
+}
+function goToRules(){
+  window.location.href = "../html/rules.html";
+}
+function goToPrep(){
+  window.location.href = "../html/prep.html";
+}
+function goToStats(){
+  window.location.href = "../html/stats.html";
+}
+function goToSettings(){
+  window.location.href = "../html/settings.html";
+}
