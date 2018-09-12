3003b1cfbee7bac8c04bd73c00bd4dc0821c3e6b
eferrer686 <edfmgr@gmail.com>
2018-09-11 21:40:56 -0500

    Basics

---
+++ Tarea-En-Equipo/Taller/js/taller.js
@@ -0,0 +1,9 @@
+function agregarVehiculo(){
+  console.log("Vehiculo agregado");
+}
+function agregarCliente(){
+  console.log("Cliente agregado");
+}
+function agregarServicio(){
+  console.log("Servicio agregado");
+}
--- Tarea-En-Equipo/Taller/src/app/app.module.ts
+++ Tarea-En-Equipo/Taller/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { VehiculoComponent } from './vehiculo/vehiculo.component';
+import { ServicioComponent } from './servicio/servicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    VehiculoComponent,
+    ServicioComponent
   ],
   imports: [
     BrowserModule
