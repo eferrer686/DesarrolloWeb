5dd09ef4477e710babc0b73be5c62c3cd03d743a
eferrer686 <edfmgr@gmail.com>
2018-09-11 22:37:03 -0500

    Json

--- Tarea-En-Equipo/Taller/js/taller.js
+++ Tarea-En-Equipo/Taller/js/taller.js
@@ -1,5 +1,14 @@
+
+
 function agregarVehiculo(){
-  console.log("Vehiculo agregado");
+  var marca = document.getElementsByName("marca")[0].value;
+  var modelo = document.getElementsByName("modelo")[0].value;
+  var año = document.getElementsByName("año")[0].value;
+  var placa = document.getElementsByName("placa")[0].value;
+
+  var coche = {Marca: marca,Modelo: modelo,Año: año,Placa: placa};
+  console.log(JSON.stringify(coche));
+
 }
 function agregarCliente(){
   console.log("Cliente agregado");
@@ -7,3 +16,22 @@ function agregarCliente(){
 function agregarServicio(){
   console.log("Servicio agregado");
 }
+
+
+var taller = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "../js/taller.json",
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+
+function printJSON(){
+  console.log(taller);
+};
